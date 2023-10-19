9e00fc12bfe9d67ef626050061e6799467b6d502
bgv102 <148369986+garverB@users.noreply.github.com>
2023-10-19 15:59:43 +0100

    First Step of the Acceleration chapter

--- ThePhysicsOfAI_m318706/Assets/Scripts/MoveShell.cs
+++ ThePhysicsOfAI_m318706/Assets/Scripts/MoveShell.cs
@@ -2,12 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MoveShell : MonoBehaviour
-{
-    public float speed = 0.5f;
-    void Update()
-    {
-        this.transform.Translate(0, Time.deltaTime * speed, Time.deltaTime);
-    }
+public class MoveShell : MonoBehaviour {
 
+    public float speed = 0.5f;
+
+
+    void Update() {
+
+        transform.Translate(0.0f, 0.0f, Time.deltaTime * speed);
+    }
 }
--- ThePhysicsOfAI_m318706/Assets/Scripts/Shell.cs
+++ ThePhysicsOfAI_m318706/Assets/Scripts/Shell.cs
@@ -4,17 +4,6 @@ using UnityEngine;
 
 public class Shell : MonoBehaviour
 {
-    public GameObject explosion;
-
-    void OnCollisionEnter(Collision col)
-    {
-        if (col.gameObject.tag == "tank")
-        {
-            GameObject exp = Instantiate(explosion, this.transform.position, Quaternion.identity);
-            Destroy(exp, 0.5f);
-            Destroy(this.gameObject);
-        }
-    }
 
     // Start is called before the first frame update
     void Start()
