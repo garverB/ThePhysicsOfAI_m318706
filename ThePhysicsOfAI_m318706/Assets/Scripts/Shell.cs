d171f5c965b1cafaae4b55bd00c074d4ca9d2693
bgv102 <148369986+garverB@users.noreply.github.com>
2023-10-19 16:23:04 +0100

    Second Step of the Acceleration chapter

--- ThePhysicsOfAI_m318706/Assets/Scripts/Shell.cs
+++ ThePhysicsOfAI_m318706/Assets/Scripts/Shell.cs
@@ -4,6 +4,21 @@ using UnityEngine;
 
 public class Shell : MonoBehaviour
 {
+    public GameObject explosion;
+    public float speed = 3.0f;
+    float mass = 10;
+    float force = 1000;
+    float acceleration;
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.tag == "tank")
+        {
+            GameObject exp = Instantiate(explosion, this.transform.position, Quaternion.identity);
+            Destroy(exp, 0.5f);
+            Destroy(this.gameObject);
+        }
+    }
 
     // Start is called before the first frame update
     void Start()
@@ -14,6 +29,9 @@ public class Shell : MonoBehaviour
     // Update is called once per frame
     void LateUpdate()
     {
-
+        acceleration = force / mass;
+        speed += acceleration * Time.deltaTime;
+        transform.Translate(0.0f, 0.0f, Time.deltaTime * speed);
     }
 }
+
